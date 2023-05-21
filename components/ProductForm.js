705925e7029d90426c2966c179c0c83806d509e4
yushever <iushina.tatiana@gmail.com>
2023-05-21 15:20:55 +0500

    images uploading + drag&drop

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,23 +1,27 @@
 import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
+import Spinner from "./Spinner";
+import { ReactSortable } from "react-sortablejs";
 
 export default function ProductForm({
   _id,
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
-  images,
+  images: existingImages,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
   const [price, setPrice] = useState(existingPrice || "");
   const [goToProducts, setGoToProducts] = useState(false);
+  const [images, setImages] = useState(existingImages || []);
+  const [isUploading, setIsUploading] = useState(false);
   const router = useRouter();
 
   async function saveProduct(e) {
     e.preventDefault();
-    const data = { title, description, price };
+    const data = { title, description, price, images };
     if (_id) {
       await axios.put("/api/products", { ...data, _id });
     } else {
@@ -29,18 +33,21 @@ export default function ProductForm({
   async function uploadImages(e) {
     const files = e.target?.files;
     if (files?.length > 0) {
+      setIsUploading(true);
       const data = new FormData();
       for (const file of files) {
         data.append("file", file);
       }
-      // const res = await axios.post("/api/upload", data, {
-      //   headers: { "Content-Type": "multipart/form-data" },
-      // });
-      const res = await fetch("/api/upload", {
-        method: "POST",
-        body: data,
+      const res = await axios.post("/api/upload", data);
+      setImages((oldImages) => {
+        return [...oldImages, ...res.data.links];
       });
+      setIsUploading(false);
     }
+  }
+
+  function updateImagesOrder(images) {
+    setImages(images);
   }
 
   if (goToProducts) {
@@ -57,7 +64,26 @@ export default function ProductForm({
         onChange={(e) => setTitle(e.target.value)}
       />
       <label>Photos</label>
-      <div className="mb-2">
+      <div className="mb-2 flex flex-wrap gap-1">
+        <ReactSortable
+          className="flex flex-wrap gap-1"
+          list={images}
+          setList={updateImagesOrder}>
+          {!!images?.length &&
+            images.map((link) => (
+              <div className="h-24 w-24" key={link}>
+                <img
+                  className="rounded-lg"
+                  src={link}
+                  alt="product image"></img>
+              </div>
+            ))}
+        </ReactSortable>
+        {isUploading && (
+          <div className="h-24 w-24 flex items-center justify-center">
+            <Spinner />
+          </div>
+        )}
         <label className="w-24 h-24 cursor-pointer border text-center flex flex-col items-center justify-center text-gray-500 rounded-lg bg-gray-200">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -75,7 +101,6 @@ export default function ProductForm({
           <div>Upload</div>
           <input type="file" onChange={uploadImages} className="hidden" />
         </label>
-        {!images?.length && <div>No photos</div>}
       </div>
       <label>Description</label>
       <textarea
---
+++ components/Spinner.js
@@ -0,0 +1,5 @@
+import { DotLoader } from "react-spinners";
+
+export default function Spinner() {
+  return <DotLoader color={"#581c87"} speedMultiplier={2} />;
+}
