ae2026ec2da16031d692c184fed54347fe6d0ea0
yushever <iushina.tatiana@gmail.com>
2023-05-22 19:28:12 +0500

    categories added

---
+++ pages/categories.js
@@ -0,0 +1,67 @@
+import Layout from "@/components/Layout";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Categories() {
+  const [name, setName] = useState("");
+  const [parentCategory, setParentCategory] = useState("");
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+  function fetchCategories() {
+    axios.get("/api/categories").then((result) => {
+      setCategories(result.data);
+    });
+  }
+  async function saveCategory(e) {
+    e.preventDefault();
+    await axios.post("/api/categories", { name, parentCategory });
+    setName("");
+    fetchCategories();
+  }
+  return (
+    <Layout>
+      <h1>Categories</h1>
+      <label>New category name</label>
+      <form onSubmit={saveCategory} className="flex gap-1">
+        <input
+          className="mb-0"
+          type="text"
+          placeholder={"Category name"}
+          value={name}
+          onChange={(e) => setName(e.target.value)}></input>
+        <select
+          className="mb-0"
+          value={parentCategory}
+          onChange={(e) => setParentCategory(e.target.value)}>
+          <option value="0">No parent category</option>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <option value={category._id}>{category.name}</option>
+            ))}
+        </select>
+        <button type="submit" className="btn-primary py-1">
+          Save
+        </button>
+      </form>
+      <table className="basic mt-4">
+        <thead>
+          <tr>
+            <td>Category name</td>
+            <td>Parent category</td>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <tr>
+                <td>{category.name}</td>
+                <td>{category?.parent?.name}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
+}
