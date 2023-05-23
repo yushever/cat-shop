8f7815b90cd48548663bef5fdec01c2f2bbd67c3
yushever <iushina.tatiana@gmail.com>
2023-05-23 20:14:34 +0500

    gitignore

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,9 +1,15 @@
 import { mongooseConnect } from "@/lib/mongoose";
 import { Category } from "@/models/Category";
+import { getServerSession } from "next-auth";
+import { authOptions, isAdminRequest } from "./auth/[...nextauth]";
 
 export default async function handle(req, res) {
   const { method } = req;
   await mongooseConnect();
+  await isAdminRequest(req, res);
+
+  const session = await getServerSession(req, res, authOptions);
+  console.log(session);
 
   if (method === "GET") {
     res.json(await Category.find().populate("parent"));
