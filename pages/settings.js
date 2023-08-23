3038a9c3f59884ea8665876c6c1384880a84c26b
yushever <iushina.tatiana@gmail.com>
2023-08-23 18:43:43 +0400

    shipping fee+promo

--- pages/settings.js
+++ pages/settings.js
@@ -7,45 +7,54 @@ import Swal from "sweetalert2";
 export default function SettingsPage() {
   const [products, setProducts] = useState([]);
   const [featuredProductId, setFeaturedProductId] = useState("");
-  const [productsLoading, setProductsLoading] = useState(false);
-  const [featuredLoading, setFeaturedLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [shippingFee, setShippingFee] = useState("");
 
   useEffect(() => {
-    setProductsLoading(true);
-    setFeaturedLoading(true);
-    axios.get("/api/products").then((res) => {
-      setProducts(res.data);
-      setProductsLoading(false);
-    });
-    axios.get("/api/settings?name=featuredProductId").then((res) => {
-      setFeaturedProductId(res.data.value);
-      setFeaturedLoading(false);
+    setIsLoading(true);
+    fetchAll().then(() => {
+      setIsLoading(false);
     });
   }, []);
 
+  async function fetchAll() {
+    await axios.get("/api/products").then((res) => {
+      setProducts(res.data);
+    });
+    await axios.get("/api/settings?name=featuredProductId").then((res) => {
+      setFeaturedProductId(res.data.value);
+    });
+    await axios.get("/api/settings?name=shippingFee").then((res) => {
+      setShippingFee(res.data.value);
+    });
+  }
+
   async function saveSettings() {
-    await axios
-      .put("/api/settings", {
-        name: "featuredProductId",
-        value: featuredProductId,
-      })
-      .then(() => {
-        Swal.fire({
-          title: "Settings saved",
-          icon: "success",
-        });
-      });
+    setIsLoading(true);
+    await axios.put("/api/settings", {
+      name: "featuredProductId",
+      value: featuredProductId,
+    });
+    await axios.put("/api/settings", {
+      name: "shippingFee",
+      value: shippingFee,
+    });
+    setIsLoading(false);
+    await Swal.fire({
+      title: "Settings saved",
+      icon: "success",
+    });
   }
 
   return (
     <Layout>
       <h1>Settings</h1>
-      {(productsLoading || featuredLoading) && (
+      {isLoading && (
         <div className="flex p-10 justify-center items-center">
           <CatLoader />
         </div>
       )}
-      {!productsLoading && !featuredLoading && (
+      {!isLoading && (
         <>
           <label>Featured product</label>
           <select
@@ -56,6 +65,11 @@ export default function SettingsPage() {
                 <option value={product._id}>{product.title}</option>
               ))}
           </select>
+          <label>Shipping price (in usd)</label>
+          <input
+            type="number"
+            value={shippingFee}
+            onChange={(e) => setShippingFee(e.target.value)}></input>
           <div>
             <button className="btn-primary" onClick={saveSettings}>
               Save settings
